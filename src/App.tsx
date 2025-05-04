01645163fb970079c9bd5ecf35a62a522cd506cd
ObadaTah <obadatahboub20@gmail.com>
2025-05-04 19:15:23 +0300

    refactored

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
 import "./App.css";
-import ClickableFace from "./components/ClickableFace";
-import ClickableText from "./components/ClickableText";
-import HapticSVG from "./components/HapticSVG";
-import imageUrl from "./assets/nice-photo.jpg";
+import UpperSection from "./components/UpperSection/UpperSection";
+
 function App() {
     return (
         <>
-            <div className="main-container">
-                <p className="main-paragraph">
-                    A
-                    <ClickableFace onClick={() => {}} image_path={imageUrl} />
-                    designer working with startups globally. Currently steering
-                    The Ship
-                    <HapticSVG />
-                    <ClickableText text="Haptic." onClick={() => {}} />
-                </p>
-            </div>
+            <UpperSection />
         </>
     );
 }
---
+++ src/components/UpperSection/UpperSection.tsx
@@ -0,0 +1,20 @@
+import ClickableFace from "./ClickableFace";
+import ClickableText from "./ClickableText";
+import HapticSVG from "./HapticSVG";
+import imageUrl from "../../assets/nice-photo.jpg";
+import "./UpperSection.css";
+const UpperSection = () => {
+    return (
+        <div className="main-container">
+            <p className="main-paragraph">
+                A
+                <ClickableFace onClick={() => {}} image_path={imageUrl} />
+                designer working with startups globally. Currently steering the
+                ship at
+                <HapticSVG />
+                <ClickableText text="Haptic." onClick={() => {}} />
+            </p>
+        </div>
+    );
+};
+export default UpperSection;
